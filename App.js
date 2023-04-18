7310ce86f9e58ed82b8862bd99e904bd4047e682
Jorge Palacios <jorge.p.codes@gmail.com>
2023-04-18 17:24:51 -0400

    Improvements using ImagePicker, but hit a wall

--- App.js
+++ App.js
@@ -1,11 +1,38 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useEffect, useState } from 'react';
+import { StyleSheet, Text, View, Button, Image } from 'react-native';
+import * as ImagePicker from 'expo-image-picker';
+import Constants from 'expo-constants';
 
 export default function App() {
+  const [image, setImage] = useState(null);
+
+  useEffect(async () => {
+    const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync();
+    if (status !== 'granted') {
+      return <Text>The app requires Media Library permissions to work!!</Text>;
+    }
+  }, []);
+
+  const selectPicture = async () => {
+    const result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      quality: 1,
+    });
+
+    console.log(result);
+
+    if (!result.canceled) {
+      setImage(result.uri);
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Button onPress={selectPicture} title='Press here' />
+      {image && <Image source={{ uri: image }} />}
+      <StatusBar style='auto' />
     </View>
   );
 }
